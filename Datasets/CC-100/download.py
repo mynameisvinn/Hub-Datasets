61fa9d6a4b9fa7983e1606ca8847b40d845d17c2
mynameisvinn <vin.tang@gmail.com>
2021-01-19 17:24:58 -0500

    cc100 url

--- Datasets/CC-100/download.py
+++ Datasets/CC-100/download.py
@@ -2,10 +2,11 @@ from cc100 import Retrieve
 
 
 def main(url):
+	print(">>")
     R = Retrieve()
     R.fetch(url)
 
 
 if __name__ == "__main__":
-    url = 'https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/historical_releases/cord-19_2020-03-13.tar.gz'
+    url = 'http://data.statmt.org/cc-100/en.txt.xz'
     main(url)
